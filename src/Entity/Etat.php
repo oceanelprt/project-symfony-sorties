9f739e216282c8ea80ec0176e181acdaade7d02b
Oceane LAPORTE <oceane.laporte2020@campus-eni.fr>
2021-10-18 15:57:23 +0200

    Add entity Etat

---
+++ src/Entity/Etat.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtatRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EtatRepository::class)
+ */
+class Etat
+{
+    const ETAT_NON_COMMENCE = 'Non commencé';
+    const ETAT_COMMENCE = 'Commencé';
+    const ETAT_TERMINE = 'Terminé';
+    const ETAT_ANNULE = 'Annulé';
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $etat;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEtat(): ?string
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(string $etat): self
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+}
