1180d92eab19a9e4d1359146394ac6c0c494ffc2
SimDub <duboelle.s@gmail.com>
2021-10-19 10:41:00 +0200

    crud sorties

---
+++ src/Form/SortieType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('duree')
+            ->add('date')
+            ->add('dateCloture')
+            ->add('nombrePlaces')
+            ->add('description')
+            ->add('createur')
+            ->add('participants')
+            ->add('lieu')
+            ->add('etat')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
