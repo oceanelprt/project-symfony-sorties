7a51a5cd68213de223bf6879ecd7fa9899f9a41f
SimDub <duboelle.s@gmail.com>
2021-10-20 23:06:22 +0200

    refacto form du controller en filtreType

--- src/Repository/SortieRepository.php
+++ src/Repository/SortieRepository.php
@@ -19,7 +19,10 @@ class SortieRepository extends ServiceEntityRepository
         parent::__construct($registry, Sortie::class);
     }
 
-    public function findSortiesByFiltre($filtre, $userId, $isOrganisateur, $isInscrit, $user)
+    /**
+     * @return Sortie les sorties en fonction des paramêtres du filtre
+     */
+    public function findSortiesByFiltre($filtre, $userId, $isOrganisateur, $isInscrit, $isNotInscrit, $isPassee, $date)
     {
         $qb = $this->createQueryBuilder('s');
 
@@ -27,36 +30,61 @@ class SortieRepository extends ServiceEntityRepository
             $qb->where('s.nom LIKE :key')
                 ->setParameter('key', '%' . $filtre['nom'] . '%');
         }
-
         if ($filtre['ville'] != '') {
             $qb->join('s.lieu', 'l', 'WITH', 'l.ville = :ville')
                 ->setParameter('ville', $filtre['ville']);
         }
-
         if ($filtre['debut'] != null) {
             $qb->andWhere('s.date >= :dateDebut')
                 ->setParameter('dateDebut', $filtre['debut']);
         }
-
         if ($filtre['fin'] != null) {
             $qb->andWhere('s.date >= :dateFin')
                 ->setParameter('dateFin', $filtre['fin']);
         }
-
         if ($isOrganisateur != null) {
             $qb->andWhere('s.createur = :user')
                 ->setParameter('user', $userId);
         }
-
-        if ($isInscrit != null) {
-            $qb->innerJoin('s.participants', 'P')
-                ->andWhere('P.id = :user')
-                ->setParameter('user', $userId);
+        if ($isInscrit != $isNotInscrit) {
+            if ($isInscrit != null) {
+                $qb->innerJoin('s.participants', 'p')
+                    ->andWhere('p.id = :user')
+                    ->setParameter('user', $userId);
+            }
+            if ($isNotInscrit != null) {
+                $qb->innerJoin('s.participants', 'n')
+                    ->andWhere('n.id != :user')
+                    ->setParameter('user', $userId);
+            }
         }
-
+        /*
+        if ($isPassee != null) {
+            $qb->andWhere('s.date' )
+               ->setParameter('user', $userId);
+        }
+        */
+        $qb->andWhere('s.date >= :now')
+            ->setParameter('now', $date->modify('-1 month'));
 
         $qb =$qb->getQuery();
 
         return $qb->getResult();
     }
+
+    /**
+     * @param $date
+     * @return Sortie Toutes les sorties jusque 1 mois après leurs réalisation
+     */
+   public function findByDate($date)
+   {
+       return $this->createQueryBuilder('s')
+           ->andWhere('s.date >= :now')
+           ->setParameter('now', $date->modify('-1 month'))
+           ->orderBy('s.date', 'ASC')
+           ->getQuery()
+           ->getResult()
+       ;
+   }
 }
+
