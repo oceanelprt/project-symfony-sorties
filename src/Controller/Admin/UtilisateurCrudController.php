7c6e12094a7fb68d3a3633762657fbc1261ee1cd
SimDub <duboelle.s@gmail.com>
2021-10-25 17:14:23 +0200

    integration de la ville dans la creation d'utilisateur dans dashboard admin

--- src/Controller/Admin/UtilisateurCrudController.php
+++ src/Controller/Admin/UtilisateurCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Utilisateur;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UtilisateurCrudController extends AbstractCrudController
 {
@@ -12,14 +15,18 @@ class UtilisateurCrudController extends AbstractCrudController
         return Utilisateur::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('pseudo'),
+            TextField::new('nom')->hideOnIndex(),
+            TextField::new('prenom')->hideOnIndex(),
+            TextField::new('telephone'),
+            EmailField::new('email')->hideOnIndex(),
+            TextField::new('password')->onlyOnForms(),
+            TextField::new('ville')->,
         ];
     }
-    */
+
 }
