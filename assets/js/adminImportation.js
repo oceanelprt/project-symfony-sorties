3fc37386144a4a043acb9f52c00d12d2f3542dd7
SimDub <duboelle.s@gmail.com>
2021-10-29 09:17:54 +0200

    nouvelle proposition adminController

--- assets/js/adminImportation.js
+++ assets/js/adminImportation.js
@@ -2,7 +2,8 @@ import $ from "jquery";
 
 $('#importation_csv_importer').ready(function () {
     $('#importation_csv_importer').click(function(){
-        $(".loader").css("display", "block");
         $("#myMusic")[0].play();
+        $(".loader").css("display", "block");
+        $("#imgAttente").css("display", "block");
     });
 });
